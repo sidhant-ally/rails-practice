2897bc637d1ef1d7b1ac6e503af369990de34cd6
sidhant-ally <sidhant@ally.io>
2021-07-07 13:18:01 +0530

    Books scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_064626) do
+ActiveRecord::Schema.define(version: 2021_07_07_074342) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.string "author"
+    t.integer "year"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "marks", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
